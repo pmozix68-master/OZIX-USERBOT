2cf667c2c8e7c40c3fbf7b69ebc484c2db2d17d5
ozixceo <pmozix0786@gmail.com>
2026-01-16 21:21:13 +0530

    Update restart.py

--- Romeo/plugins/restart.py
+++ Romeo/plugins/restart.py
@@ -9,5 +9,5 @@ from Romeo import SUDO_USER
 async def restart(client: Client, message: Message):
     reply = await message.reply_text("**Restarting...**")
     await message.delete()
-    await reply.edit_text("Successfully Restarted RomeoBot...\n\n💞 Wait 1-2 minutes\nLoad plugins...</b>")
+    await reply.edit_text("Successfully Restarted OzixBot...\n\n💞 Wait 1-2 minutes\nLoad plugins...</b>")
     os.system(f"kill -9 {os.getpid()} && python3 -m Romeo")
